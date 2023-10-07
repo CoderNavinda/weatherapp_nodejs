285c4d17d08e4c4f4bb884101a12ec7f29c80ff6
Navinda Perera <72337893+CoderNavinda@users.noreply.github.com>
2023-10-07 11:29:21 +0530

    Update app.js
    did some commenting

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
-app.set("view engine", "ejs"); // Set EJS as the view engine
+app.set("view engine", "ejs"); 
 
 app.get("/", (req, res) => {
-  // Replace 'YOUR_API_KEY' with an actual API key from a weather service provider
-  const apiKey = "bea07a938abe2ed303593b462a30fb2f"; // Replace with your actual OpenWeatherMap API key
+ 
+  const apiKey = "bea07a938abe2ed303593b462a30fb2f"; // Replace with your OpenWeatherMap API key
   const city = "New York"; // Replace with the desired city or location
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -19,10 +19,10 @@ app.get("/", (req, res) => {
     if (!error && response.statusCode === 200) {
       const weatherData = JSON.parse(body);
       console.log(weatherData);
-      res.render("index", { weatherData }); // Render the EJS template
+      res.render("index", { weatherData }); 
     } else {
       const errorHtml = "<p>Error fetching weather data.</p>";
-      res.render("index", { errorHtml }); // Render the EJS template with an error
+      res.render("index", { errorHtml });
     }
   });
 });
